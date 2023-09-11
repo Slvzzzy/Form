e95f0644503da371ffa7672ee18c8892613e5216
Slvzzzy <dexiar77@gmail.com>
2023-09-11 22:32:38 +0700

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,8 @@ import {MainPageModule} from "./main-page/main-page.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
--- src/app/main-page/main-page.module.ts
+++ src/app/main-page/main-page.module.ts
@@ -2,23 +2,34 @@ import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {PageComponent} from "./page/page.component";
 import {RouterModule, Routes} from "@angular/router";
-
+import {MatIconModule} from '@angular/material/icon'
+import {NavbarPageComponent} from "./page/navbar-page/navbar-page.component";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatDividerModule} from "@angular/material/divider";
+import {MatButtonModule} from "@angular/material/button";
 const routes: Routes = [
   {path: 'main-page', component: PageComponent}
 ]
 
 @NgModule({
   declarations: [
-    PageComponent
+    PageComponent,
+    NavbarPageComponent
   ],
   imports: [
     CommonModule,
     RouterModule.forRoot(routes),
+    MatIconModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatDividerModule,
+    MatButtonModule
   ],
   exports: [
     PageComponent,
-    RouterModule
-
+    RouterModule,
+    NavbarPageComponent
   ]
 })
 export class MainPageModule {
